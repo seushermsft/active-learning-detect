5951a793a133da462af66e5e90a0b2b624e1efc1
Mihaela Curmei <curmeimihaela@gmail.com>
2018-10-11 09:30:42 -0700

    fixing setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='pyfunction',
     version='0.1dev',
-    packages=['HttpTrigger'],
+    packages=['MyFunctionProj'],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     include_package_data=True,
 )

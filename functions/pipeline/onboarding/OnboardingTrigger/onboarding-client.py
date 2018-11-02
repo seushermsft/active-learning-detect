c73270646f9d09c257387d90c6f361c7c7de7b5f
Amanda Kaufman <aka@microsoft.com>
2018-11-02 12:18:51 -0600

    Added response status code to output for testing.

--- functions/pipeline/onboarding/OnboardingTrigger/onboarding-client.py
+++ functions/pipeline/onboarding/OnboardingTrigger/onboarding-client.py
@@ -1,9 +1,13 @@
 import requests
 import json
 
+# The following mock client imitates the CLI during the onboarding scenario for new images.
+# The CLI uploads images to a temporary blob store, then gets a list of URLs to those images and
+# passes it to an HTTP trigger function, which calls the DAL to create rows in the database.
+
 print("\nTest client for CLI Onboarding scenario\n-------------------------------------\n")
 
-print("Now executing cURL POST request to onboard images...")
+print("Now executing POST request to onboard images...")
 
 functionURL = "https://mtarngfunc-test.azurewebsites.net/api/onboarding?code=ezQ4rW/DvaNtm6DlHZ7XQp1Enrtao3WpsW4FFR5V/nFLVb9vq4P7PQ=="
 headers = {"Content-Type": "application/json"}
@@ -19,4 +23,5 @@ raw_response = response.text
 response_array = raw_response.split(", ")
 response_output = "\n".join(response_array)
 
+print("Response status code: " + str(response.status_code))
 print("Response: " + response_output)

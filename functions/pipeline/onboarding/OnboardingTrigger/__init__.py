e0b173a5ab81aa576c6368488d68f901b09de2df
Amanda Kaufman <aka@microsoft.com>
2018-11-05 16:48:50 -0700

    Added code to copy image from temp storage into permanent storage. Copies image as new filename == ImageID from row created in DB. Note: Changes not yet tested.

--- functions/pipeline/onboarding/OnboardingTrigger/__init__.py
+++ functions/pipeline/onboarding/OnboardingTrigger/__init__.py
@@ -1,6 +1,7 @@
+import os
 import logging
 import azure.functions as func
-
+from shutil import copyfile
 from ...shared import db_access as DB_Access
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
@@ -41,11 +42,15 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     # Receive dictionary of mapped { ImageID : ImageURL }
     image_id_url_map = DB_Access.get_image_ids_for_new_images(db, image_object_list)
 
-    # Update image names in temporary blob store to reflect new ImageID returned from previous function
-    
-
     # Copy over images to permanent blob store, get list of URLs to iamges in permanent storage
-
+    # Iterating over a dictionary in Python: https://stackoverflow.com/questions/3294889/iterating-over-dictionaries-using-for-loops
+    # Syntax for copying a file in Python: copyfile(src, dst)
+    # TODO: Figure out how to parse final filename: https://stackoverflow.com/questions/26443308/find-last-occurrence-of-character-in-string-python
+    for key, value in image_id_url_map.items():
+        file_extension = os.path.splitext(value)[1]
+        permanent_storage_directory = "some_storage_url"
+        permanent_storage_path = permanent_storage_directory + "/" + key + file_extension
+        copyfile(value, permanent_storage_path)
 
     # Close connection to database.
     db.close()
